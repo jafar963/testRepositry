d03d74204d0726eebb7383fc1a80d40db4315917
jafar.haldien <jafar.haldien@dna-tec.de>
2022-09-08 21:18:09 +0300

    added database access

--- php-mvc/application/config/config.php
+++ php-mvc/application/config/config.php
@@ -6,7 +6,7 @@
  * For more info about constants please @see http://php.net/manual/en/function.define.php
  * If you want to know why we use "define" instead of "const" @see http://stackoverflow.com/q/2447791/1114320
  */
-
+ 
 /**
  * Configuration for: Error reporting
  * Useful to show every little problem during development, but only show hard errors in production
